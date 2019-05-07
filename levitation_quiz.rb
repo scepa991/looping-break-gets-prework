c6958b120dbe0813b45fa34342c78053d68a8627
Stefan Scepanovic <scepa91@icloud.com>
2019-05-07 16:23:41 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,5 +1,11 @@
 
 def levitation_quiz
+  loop do
+    puts "Whats up, Doc?"
+    answer = gets.chomp
+    break if answer == "STOP"
+  end
+  puts "Okay, okay, jeez. I'll stop. Sorry."
 	#your code here
 end
 
